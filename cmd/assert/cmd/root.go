37c35739e2b9fc09785e06ff24796ed37907cb15
Philzon <7958046+philzon@users.noreply.github.com>
2020-12-22 14:13:18 +0100

    Print exact YAML issue in configuration

--- cmd/assert/cmd/root.go
+++ cmd/assert/cmd/root.go
@@ -126,7 +126,7 @@ func initConfig(configPath string, skips []string) (*config.Config, error) {
 	err = yaml.Unmarshal(data, cfg)
 
 	if err != nil {
-		return nil, fmt.Errorf("could not parse config YAML: %s", err.Error())
+		return nil, fmt.Errorf("could not parse config YAML: %s", yaml.FormatError(err, false, true))
 	}
 
 	// Put all skip strings in a map and iterate all tests and enable
