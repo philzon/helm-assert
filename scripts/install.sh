2bc6964e28a4ae5df2f1d30283150ab03ca1519e
Philzon <7958046+philzon@users.noreply.github.com>
2021-03-13 17:45:38 +0100

    Get release latest version from Github API
    Using 'plugin.yaml' would not work if the version was set to a version
    not yet released i.e. updating the version to new development iteration
    for each release.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 NAME="$(cat plugin.yaml | grep "name" | cut -d '"' -f 2)"
-VERSION="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
+VERSION="$(curl --silent https://api.github.com/repos/philzon/helm-assert/releases/latest | grep tag_name | sed -E 's/.*"([^"]+)".*/\1/')"
 KERNEL="$(uname -s | tr '[:upper:]' '[:lower:]')"
 ARCH="$(uname -m)"
 
@@ -15,21 +15,21 @@ then
 	ARCH="amd64"
 fi
 
-URL="https://github.com/philzon/helm-assert/releases/download/v${VERSION}/${NAME}-v${VERSION}-${KERNEL}-${ARCH}.tar.gz"
+URL="https://github.com/philzon/helm-assert/releases/download/${VERSION}/${NAME}-${VERSION}-${KERNEL}-${ARCH}.tar.gz"
 
 echo "${URL}"
 
 mkdir --parent "bin"
-mkdir --parent "releases/v${VERSION}"
+mkdir --parent "releases/${VERSION}"
 
 if [ -x "$(which curl 2>/dev/null)" ]
 then
-	curl -sSL "${URL}" -o "releases/v${VERSION}.tar.gz"
+	curl -sSL "${URL}" -o "releases/${VERSION}.tar.gz"
 else
-	wget -q "${URL}" -O "releases/v${VERSION}.tar.gz"
+	wget -q "${URL}" -O "releases/${VERSION}.tar.gz"
 fi
 
-tar -xzf "releases/v${VERSION}.tar.gz" -C "releases/v${VERSION}"
-mv "releases/v${VERSION}/${NAME}" "bin/${NAME}" || \
-	mv "releases/v${VERSION}/${NAME}.exe" "bin/${NAME}"
-mv "releases/v${VERSION}/plugin.yaml" .
+tar -xzf "releases/${VERSION}.tar.gz" -C "releases/${VERSION}"
+mv "releases/${VERSION}/${NAME}" "bin/${NAME}" || \
+	mv "releases/${VERSION}/${NAME}.exe" "bin/${NAME}"
+mv "releases/${VERSION}/plugin.yaml" .
